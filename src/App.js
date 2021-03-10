6b8d6c5a32754ae7060a841069e8407b83dbb35b
Sammy Osborne <osbee10@gmail.com>
2021-03-10 14:06:17 -0800

    osborne

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import MyFirstComponent from './myfirstcomponent'
-import MySecondComponent from './mysecondcomponent'
-import MyThirdComponent from './mythirdcomponent'
-import MyFourthComponet from './myfourthcomponent'
-import MyFithComponent from './myfithcomponent'
-import MySixthComponent from './mysixthcomponent'
-import MySeventhComponent from './myseventhcomponent'
-import MyEightComponent from './myeightcomponent'
-import MyNinthComponent from './myninthcomponent'
-import MyTenthComponent from './mytenthcomponent'
-// import MasterComponent from './MasterComponents'
-// import ParentComponent from './parentcomponent'
+import MyFirstComponent from './components/myfirstcomponent'
+import MySecondComponent from './components/mysecondcomponent'
+import MyThirdComponent from './components/mythirdcomponent'
+import MyFourthComponet from './components/myfourthcomponent'
+import MyFithComponent from './components/myfithcomponent'
+import MySixthComponent from './components/mysixthcomponent'
+import MySeventhComponent from './components/myseventhcomponent'
+import MyEightComponent from './components/myeightcomponent'
+import MyNinthComponent from './components/myninthcomponent'
+import MyTenthComponent from './components/mytenthcomponent'
+import MasterComponent from './components/MasterComponents'
+import ParentComponent from './components/parentcomponent'
+import ProfileComponent from './components/profilecomponent'
 
 function App() {
   
   return (
     <div>
-      <MyFirstComponent />
+      {/* <MyFirstComponent />
       <MySecondComponent />
       <MyThirdComponent />
       <MyFourthComponet />
@@ -24,13 +25,14 @@ function App() {
       <MySeventhComponent />
       <MyEightComponent />
       <MyNinthComponent />
-      <MyTenthComponent />
-      {/* <MasterComponent name='1' height='45' />
+      <MyTenthComponent /> */}
+      <MasterComponent name='1' height='45' />
       <MasterComponent name='2' height='67' />
       <MasterComponent name='3' height='34' />
-      <MasterComponent name='4' height='38' />  */}
+      <MasterComponent name='4' height='38' /> 
 
-      {/*<ParentComponent name="Kojo" gender="Female" age="23" email="asinsjg@gmala.com"/> */}
+      <ParentComponent name="Kojo" gender="Female" age="23" email="asinsjg@gmala.com"/>
+      <ProfileComponent firstname='Sammy' lastname='Osborne' email='osee10@gmail.com' dateofbirth='15/15/01' mobile='0273315746'/>
     </div>
   );
 }
